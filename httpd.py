315b03ddcb3cc385b2af3c1290e16b6e32f78bfb
enetrebko <enetrebko@gmail.com>
2020-08-26 21:37:22 +0300

    fixes

--- httpd.py
+++ httpd.py
@@ -2,37 +2,21 @@ import socket
 import threading
 import os
 from urllib.parse import unquote
-from collections import namedtuple
 from datetime import datetime
 import argparse
+import time
+from http import HTTPStatus
+import mimetypes
 
 
 HOST = "0.0.0.0"
 PORT = 8080
-QUEUE_SIZE = 5
+QUEUE_SIZE = 10
 DOCUMENT_ROOT = 'root'
 DEFAULT_FILE = 'index.html'
 WORKERS_COUNT = 5
 TIMEOUT = 60
-
-CONTENT_TYPES = {
-    "html": "text/html",
-    "png": "image/png",
-    "css": "text/css",
-    "txt": "text/html",
-    "jpg": "image/jpeg",
-    "jpeg": "image/jpeg",
-    "swf": "application/x-shockwave-flash",
-    "js": "application/javascript",
-    "gif": "image/gif",
-}
-
-STATUS_MESSAGE = {
-    200: "OK",
-    403: "Forbidden",
-    404: "Not Found",
-    405: "Not Allowed"
-}
+REQUEST_MAX_SIZE = 10 * 1024
 
 
 class HTTPServer:
@@ -41,70 +25,81 @@ class HTTPServer:
         self.port = port
         self.workers_count = workers_count
         self.document_root = document_root
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def get_request(self, client_socket):
         request = b""
-        while not (b"\r\n\r\n" in request or b"\n\n" in request):
-            request += client_socket.recv(65536)
+        while not (b"\r\n\r\n" in request or b"\n\n" in request or len(request) > REQUEST_MAX_SIZE):
+            chunk = client_socket.recv(1024)
+            if not chunk:
+                break
+            request += chunk
         return request
 
     def handle_client_connection(self, client_sock):
-        request = HTTPRequest(self.get_request(client_sock))
-        response = HTTPResponse(request)
-        client_sock.sendall(response.generate_response())
-        client_sock.close()
+        with client_sock:
+            request = HTTPRequest(self.get_request(client_sock))
+            response = HTTPResponse(request)
+            client_sock.sendall(response.generate_response())
+
+    def wait_connection(self, sock):
+        while True:
+            client_sock, address = sock.accept()
+            client_sock.settimeout(TIMEOUT)
+            self.handle_client_connection(client_sock)
 
     def start(self):
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((self.host, self.port))
-        self.server.listen(QUEUE_SIZE)
-        for _ in range(self.workers_count):
-            while True:
-                client_sock, address = self.server.accept()
-                client_sock.settimeout(TIMEOUT)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen(QUEUE_SIZE)
+            for _ in range(self.workers_count):
                 client_handler = threading.Thread(
-                    target=self.handle_client_connection,
-                    args=(client_sock,)
+                    target=self.wait_connection,
+                    args=(sock,)
                 )
                 client_handler.setDaemon(True)
                 client_handler.start()
+                try:
+                    while True:
+                        time.sleep(1)
+                except KeyboardInterrupt:
+                    print("Interrupted by user")
+                    return
 
 
 class HTTPRequest:
     def __init__(self, request):
         self.request = request
-        self.data = self.get_data()
-        self.method = self.get_method()
-        self.url_info = self.parse_url()
-        self.request_url = self.url_info.url
-        self.file_ext = self.url_info.file_ext
-        self.file_status = self.url_info.file_status
+        self.request_url, self.file_ext, self.status, self.method = self.parse_request()
 
-    def get_data(self):
-        return self.request.decode().split('\r\n')[0]
+    def parse_request(self):
+        request_url = file_ext = method = None
+        status = HTTPStatus.OK
+        try:
+            data = self.request.decode().split('\r\n')[0]
+            method = data.split()[0]
+            url = data.split()[1]
+            request_url, file_ext = self.parse_url(url)
+            if not request_url:
+                status = HTTPStatus.NOT_FOUND
+        except:
+            status = HTTPStatus.BAD_REQUEST
+        finally:
+            return request_url, file_ext, status, method
 
-    def get_method(self):
-        return self.data.split()[0]
-
-    def get_url(self):
-        return self.data.split()[1]
-
-    def parse_url(self):
-        url = self.get_url()
-        url_info = namedtuple("url_info", "url file_ext file_status")
-        if "?" in url:
-            url = url[:url.find("?")]
-        url = unquote(url)
-        url = os.path.join(DOCUMENT_ROOT, url.replace("/", "", 1))
-        file_ext = url.split(".")[-1]
-        if url.find("../../") > 0:
-            return url_info(url, file_ext, "root escaping")
-        if os.path.isdir(url):
-            url = os.path.join(url, DEFAULT_FILE)
-        if os.path.isfile(url):
-            return url_info(url, file_ext, "ok")
-        return url_info(url, file_ext, "not found")
+    def parse_url(self, url):
+        url_norm = url.split("?")[0]
+        url_norm = unquote(url_norm)
+        url_norm = os.path.abspath(url_norm)
+        url_norm = os.path.join(DOCUMENT_ROOT, url_norm.lstrip("/"))
+        if url.endswith("/"):
+            url_norm += "/"
+        file_ext = os.path.splitext(url_norm)[-1]
+        if os.path.isdir(url_norm):
+            url_norm = os.path.join(url_norm, DEFAULT_FILE)
+        if os.path.isfile(url_norm):
+            return url_norm, file_ext
+        return None, None
 
 
 class HTTPResponse:
@@ -115,16 +110,12 @@ class HTTPResponse:
 
     def get_status(self):
         if self.request.method == "POST":
-            return 405
-        if self.request.file_status == "not found":
-            return 404
-        if self.request.file_status == "root escaping":
-            return 403
+            return HTTPStatus.METHOD_NOT_ALLOWED
         else:
-            return 200
+            return self.request.status
 
     def get_message(self):
-        return STATUS_MESSAGE.get(self.status)
+        return self.status.phrase
 
     def get_body(self):
         with open(self.request.request_url, 'rb') as f:
@@ -132,19 +123,25 @@ class HTTPResponse:
 
     def generate_response(self):
         head = "HTTP/1.1"
-        content_type = CONTENT_TYPES.get(self.request.file_ext, "text/html")
+        content_type = mimetypes.types_map.get(self.request.file_ext, "text/html")
         date_time = datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
         server_name = "MyServer"
-        response = f"{head} {self.status} {self.message}\r\n" + \
-                   f"Date: {date_time}\r\n" + \
-                   f"Server:{server_name}\r\n" + \
-                   "Connection: close\r\n"
-        if self.status != 200:
+        response = "\r\n".join([
+            f"{head} {self.status} {self.message}",
+            f"Date: {date_time}",
+            f"Server:{server_name}",
+            "Connection: close",
+            ""]
+        )
+        if self.status != HTTPStatus.OK:
             return response.encode()
         body = self.get_body()
         content_length = len(body)
-        response += f"Content-Length: {content_length}\r\n" + \
-                    f"Content-Type: {content_type}\r\n\r\n"
+        response += "\r\n".join([
+            f"Content-Length: {content_length}",
+            f"Content-Type: {content_type}"
+        ])
+        response += "\r\n\r\n"
         response = response.encode()
         if self.request.method == 'GET':
             response += body
